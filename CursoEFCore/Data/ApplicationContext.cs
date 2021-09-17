fcf5321a436875c7d6beaff2be47c1f988621d1c
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2021-09-16 23:43:53 -0300

    Adicionar arquivos de projeto.

---
+++ CursoEFCore/Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoEFCore.Data
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<Pedido> Pedidos { get; set; } 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = localhost\SQLEXPRESS; Initial Catalog = CursoEFCore; Integrated Security = True; Connect Timeout = 30");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+
+        }
+    }
+}
